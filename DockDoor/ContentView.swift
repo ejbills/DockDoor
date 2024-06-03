488bc1727bd69157ddf7fb61a207fb2fea777fcc
Ethan Bills <ethanjacksonbills@gmail.com>
2024-06-03 10:17:42 -0700

    First commit

---
+++ DockDoor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DockDoor
+//
+//  Created by Ethan Bills on 6/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DockDoor/DockDoorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DockDoorApp.swift
+//  DockDoor
+//
+//  Created by Ethan Bills on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct DockDoorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
