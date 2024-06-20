4fce46eed73dc9ee1933886f0ddc059b6c87579a
Ethan Bills <ethanjacksonbills@gmail.com>
2024-06-20 13:45:09 -0700

    launch on login + settings changes

--- DockDoor/Views/Settings/SettingsView.swift
+++ DockDoor/Views/Settings/SettingsView.swift
@@ -7,39 +7,55 @@
 
 import SwiftUI
 import Defaults
+import LaunchAtLogin
 
 struct SettingsView: View {
     var body: some View {
-        VStack(spacing: 20) {
+        VStack(alignment: .leading, spacing: 5) {
+            LaunchAtLogin.Toggle("Launch DockDoor at login")
             SizePickerView()
         }
-        .padding()
+        .padding(20)
     }
 }
 
 struct SizePickerView: View {
     @Default(.sizingMultiplier) var sizingMultiplier
-
+    
     var body: some View {
-        VStack {
-            Text("Select Size:")
-                .font(.headline)
-
-            Picker("Size", selection: $sizingMultiplier) {
+        VStack(spacing: 20) {
+            Picker("Window Size", selection: $sizingMultiplier) {
                 ForEach(2...7, id: \.self) { size in
-                    Text("\(size)").tag(CGFloat(size))
+                    Text(getLabel(for: CGFloat(size))).tag(CGFloat(size))
                 }
             }
-            .pickerStyle(MenuPickerStyle())
             .onChange(of: sizingMultiplier) { _, newValue in
-                MessageUtil.showMessage(title: "Restart required.", message: "Please restart the application to apply your changes. Click OK to quit the app.", completion: { _ in quitApp() })
+                MessageUtil.showMessage(title: "Restart required.", message: "Please restart the application to apply your changes. Click OK to quit the app.", completion: { result in
+                    if result == .ok {
+                        quitApp()
+                    }
+                })
             }
-
-            Text("Current Size: \(Int(sizingMultiplier))")
-                .font(.subheadline)
-                .padding(.top, 10)
         }
-        .padding()
+        .padding(.vertical, 20)
+    }
+    
+    private func getLabel(for size: CGFloat) -> String {
+        switch size {
+        case 2:
+            return "Large"
+        case 3:
+            return "Default (Medium Large)"
+        case 4:
+            return "Medium"
+        case 5:
+            return "Small"
+        case 6:
+            return "Extra Small"
+        case 7:
+            return "Extra Extra Small"
+        default:
+            return "Unknown Size"
+        }
     }
 }
-
