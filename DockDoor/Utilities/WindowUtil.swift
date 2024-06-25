61e2a865e6c5c2660da59ff93dc5600dcdfc7d5d
Ethan Bills <ethanjacksonbills@gmail.com>
2024-06-24 20:33:22 -0700

    clean up window util

--- DockDoor/Utilities/WindowUtil.swift
+++ DockDoor/Utilities/WindowUtil.swift
@@ -9,93 +9,89 @@ import Cocoa
 import ApplicationServices
 import ScreenCaptureKit
 
+/// Struct representing window information.
 struct WindowInfo: Identifiable, Hashable {
     let id: CGWindowID
-    let window: SCWindow
+    let window: SCWindow?
     let appName: String
     let bundleID: String
     let windowName: String?
     var image: CGImage?
     var axElement: AXUIElement
     var closeButton: AXUIElement?
+    let isMinimized: Bool
 }
 
-// Cache item structure
+/// Cache item structure for storing captured window images.
 struct CachedImage {
     let image: CGImage
     let timestamp: Date
 }
 
+/// Cache item structure for storing app icons.
 struct CachedAppIcon {
     let icon: NSImage
     let timestamp: Date
 }
 
 struct WindowUtil {
-    
     private static var imageCache: [CGWindowID: CachedImage] = [:]
     private static var iconCache: [String: CachedAppIcon] = [:]
     private static let cacheQueue = DispatchQueue(label: "com.dockdoor.cacheQueue", attributes: .concurrent)
-
     private static var cacheExpirySeconds: Double = 600 // 10 mins
     
+    // MARK: - Cache Management
+    
+    /// Clears expired cache items based on cache expiry time.
     static func clearExpiredCache() {
         let now = Date()
         cacheQueue.async(flags: .barrier) {
-            imageCache = imageCache.filter { now.timeIntervalSince($0.value.timestamp) <= cacheExpirySeconds / 10 }
+            imageCache = imageCache.filter { now.timeIntervalSince($0.value.timestamp) <= cacheExpirySeconds }
             iconCache = iconCache.filter { now.timeIntervalSince($0.value.timestamp) <= cacheExpirySeconds }
         }
     }
     
+    /// Resets the image and icon cache.
+    static func resetCache() {
+        imageCache.removeAll()
+        iconCache.removeAll()
+    }
+    
     // MARK: - Helper Functions
     
+    /// Captures the image of a given window.
     static func captureWindowImage(windowInfo: WindowInfo) async throws -> CGImage {
         clearExpiredCache()
-        
         return try cacheQueue.sync(flags: .barrier) {
-            if let cachedImage = imageCache[windowInfo.id],
-               Date().timeIntervalSince(cachedImage.timestamp) <= cacheExpirySeconds {
+            if let cachedImage = imageCache[windowInfo.id], Date().timeIntervalSince(cachedImage.timestamp) <= cacheExpirySeconds {
                 return cachedImage.image
             }
-            
             guard CGPreflightScreenCaptureAccess() else {
-                print("Debug: Screen recording permission not granted")
-                MessageUtil.showMessage(title: "Permission error",
-                                        message: "You need to give DockDoor access to Screen Recording in Security & Privacy for it to function.",
-                                        completion: { _ in SystemPreferencesHelper.openScreenRecordingPreferences() })
                 throw NSError(domain: "com.dockdoor.permission", code: 2, userInfo: [NSLocalizedDescriptionKey: "Screen recording permission not granted"])
             }
-            
-            let id = windowInfo.id
-            let frame = windowInfo.window.frame
-            
-            guard let image = CGWindowListCreateImage(frame, .optionIncludingWindow, id, [.boundsIgnoreFraming, .bestResolution]) else {
+            guard let frame = windowInfo.window?.frame,
+                let image = CGWindowListCreateImage(frame, .optionIncludingWindow, windowInfo.id, [.boundsIgnoreFraming, .bestResolution]) else {
                 throw NSError(domain: "com.dockdoor.error", code: 1, userInfo: [NSLocalizedDescriptionKey: "Failed to capture window image"])
             }
-            
             let cachedImage = CachedImage(image: image, timestamp: Date())
             imageCache[windowInfo.id] = cachedImage
-            
             return image
         }
     }
     
+    /// Creates an AXUIElement for a given process ID.
     static func createAXUIElement(for pid: pid_t) -> AXUIElement {
         return AXUIElementCreateApplication(pid)
     }
     
+    /// Retrieves the AXUIElement windows for an application reference.
     static func getAXWindows(for appRef: AXUIElement) -> [AXUIElement]? {
         var windowList: AnyObject?
         let result = AXUIElementCopyAttributeValue(appRef, kAXWindowsAttribute as CFString, &windowList)
-        
-        if result != .success {
-            print("Error getting windows: \(result.rawValue)")
-            return nil
-        }
-        
-        return windowList as? [AXUIElement]
+        return result == .success ? windowList as? [AXUIElement] : nil
     }
     
+    /// Finds a window by its name in the provided AXUIElement windows.
     static func findWindow(byName windowName: String, in windows: [AXUIElement]) -> AXUIElement? {
         for windowRef in windows {
             var windowTitleValue: AnyObject?
@@ -107,102 +103,143 @@ struct WindowUtil {
         return nil
     }
     
+    /// Retrieves the close button for a given window reference.
     static func getCloseButton(for windowRef: AXUIElement) -> AXUIElement? {
         var closeButton: AnyObject?
         let result = AXUIElementCopyAttributeValue(windowRef, kAXCloseButtonAttribute as CFString, &closeButton)
-        if result == .success {
-            let closeButtonUIElement = closeButton as! AXUIElement
-            return closeButtonUIElement
-        }
-        return nil
+        let closeButtonElement = closeButton as! AXUIElement
+        return result == .success ? closeButtonElement : nil
+    }
+    
+    /// Retrieves the running application by its name.
+    static func getRunningApplication(named applicationName: String) -> NSRunningApplication? {
+        return NSWorkspace.shared.runningApplications.first { $0.localizedName == applicationName }
     }
     
     // MARK: - Window Manipulation Functions
     
+    /// Toggles the minimize state of a window.
+    static func toggleMinimize(windowInfo: WindowInfo) {
+        let minimizeResult: AXError = windowInfo.isMinimized ?
+            AXUIElementSetAttributeValue(windowInfo.axElement, kAXMinimizedAttribute as CFString, kCFBooleanFalse) :
+            AXUIElementSetAttributeValue(windowInfo.axElement, kAXMinimizedAttribute as CFString, kCFBooleanTrue)
+        
+        if minimizeResult != .success {
+            print("Error toggling minimized state of window: \(minimizeResult.rawValue)")
+        }
+    }
+    
+    /// Brings a window to the front and focuses it.
     static func bringWindowToFront(windowInfo: WindowInfo) {
         let raiseResult = AXUIElementPerformAction(windowInfo.axElement, kAXRaiseAction as CFString)
         let focusResult = AXUIElementSetAttributeValue(windowInfo.axElement, kAXFocusedAttribute as CFString, kCFBooleanTrue)
         let frontmostResult = AXUIElementSetAttributeValue(windowInfo.axElement, kAXFrontmostAttribute as CFString, kCFBooleanTrue)
-        let activateResult = NSRunningApplication(processIdentifier: windowInfo.window.owningApplication?.processID ?? 0)?.activate()
+        let activateResult = NSRunningApplication(processIdentifier: windowInfo.window?.owningApplication?.processID ?? 0)?.activate()
         
-        if raiseResult == .success && focusResult == .success && frontmostResult == .success && activateResult == true {
-            print("Debug: Successfully raised, focused, and activated window")
-        } else {
-            print("Error bringing window to front. Raise result: \(raiseResult.rawValue), Focus result: \(focusResult.rawValue), Frontmost result: \(frontmostResult), Activate result: \(String(describing: activateResult))")
-            AXUIElementSetAttributeValue(windowInfo.axElement, kAXFrontmostAttribute as CFString, kCFBooleanTrue)
-            NSRunningApplication(processIdentifier: windowInfo.window.owningApplication?.processID ?? 0)?.activate(options: [.activateAllWindows])
+        if activateResult != true || raiseResult != .success || focusResult != .success {
+            let fallbackActivateResult = NSRunningApplication(processIdentifier: windowInfo.window?.owningApplication?.processID ?? 0)?.activate(options: [.activateAllWindows])
+            let fallbackResult = AXUIElementSetAttributeValue(windowInfo.axElement, kAXFrontmostAttribute as CFString, kCFBooleanTrue)
+            
+            if fallbackActivateResult != true || fallbackResult != .success {
+                print("Failed to bring window to front with fallback attempts.")
+            }
         }
     }
     
+    /// Closes a window using its close button.
     static func closeWindow(closeButton: AXUIElement) {
         let closeResult = AXUIElementPerformAction(closeButton, kAXPressAction as CFString)
-        if closeResult == .success {
-            print("Debug: Successfully closed window")
-        } else {
+        if closeResult != .success {
             print("Error closing window: \(closeResult.rawValue)")
         }
     }
     
-    static func resetCache() {
-        imageCache.removeAll()
-        iconCache.removeAll()
+    // MARK: - Minimized Window Handling
+    
+    /// Retrieves minimized windows' information for a given process ID, bundle ID, and app name.
+    static func getMinimizedWindows(pid: pid_t, bundleID: String, appName: String) -> [WindowInfo] {
+        var minimizedWindowsInfo: [WindowInfo] = []
+        let appElement = AXUIElementCreateApplication(pid)
+        var value: AnyObject?
+        let result = AXUIElementCopyAttributeValue(appElement, kAXWindowsAttribute as CFString, &value)
+
+        if result == .success, let windows = value as? [AXUIElement] {
+            for (index, window) in windows.enumerated() {
+                var minimizedValue: AnyObject?
+                let minimizedResult = AXUIElementCopyAttributeValue(window, kAXMinimizedAttribute as CFString, &minimizedValue)
+                if minimizedResult == .success, let isMinimized = minimizedValue as? Bool, isMinimized {
+                    let windowName: String? = getAXAttribute(element: window, attribute: kAXTitleAttribute as CFString)
+                    let windowInfo = WindowInfo(id: UInt32(index), window: nil, appName: appName, bundleID: bundleID,
+                                                windowName: windowName, image: nil, axElement: window,
+                                                closeButton: nil, isMinimized: true)
+                    minimizedWindowsInfo.append(windowInfo)
+                }
+            }
+        }
+        return minimizedWindowsInfo
     }
     
-    // Utility function to list active windows for a specific application
+    /// Retrieves a value for a given AXUIElement attribute.
+    static func getAXAttribute<T>(element: AXUIElement, attribute: CFString) -> T? {
+        var value: CFTypeRef?
+        let result = AXUIElementCopyAttributeValue(element, attribute, &value)
+        return result == .success ? value as? T : nil
+    }
+   
+    // MARK: - Active Window Handling
+    
+    /// Retrieves the active windows for a given application name.
     static func activeWindows(for applicationName: String) async throws -> [WindowInfo] {
         let content = try await SCShareableContent.excludingDesktopWindows(true, onScreenWindowsOnly: true)
-        let group = LimitedTaskGroup<WindowInfo?>(maxConcurrentTasks: 4) // Adjust this number as needed
+        let group = LimitedTaskGroup<WindowInfo?>(maxConcurrentTasks: 4)
+        var foundApp: SCRunningApplication?
         
         for window in content.windows {
-            if let app = window.owningApplication,
-               applicationName.isEmpty || (app.applicationName.contains(applicationName) && !applicationName.isEmpty) {
+            if let app = window.owningApplication, applicationName.isEmpty || (app.applicationName.contains(applicationName) && !applicationName.isEmpty) {
                 await group.addTask {
-                    try await fetchWindowInfo(window: window, applicationName: applicationName)
+                    return try await fetchWindowInfo(window: window, applicationName: applicationName)
                 }
+                foundApp = app
+            }
+        }
+        
+        if let app = foundApp, !applicationName.isEmpty {
+            let minimizedWindowsInfo = getMinimizedWindows(pid: app.processID, bundleID: app.bundleIdentifier, appName: applicationName)
+            for windowInfo in minimizedWindowsInfo {
+                await group.addTask { return windowInfo }
+            }
+        } else if !applicationName.isEmpty, let app = getRunningApplication(named: applicationName) {
+            let minimizedWindowsInfo = getMinimizedWindows(pid: app.processIdentifier, bundleID: app.bundleIdentifier!, appName: applicationName)
+            for windowInfo in minimizedWindowsInfo {
+                await group.addTask { return windowInfo }
             }
         }
         
         let results = try await group.waitForAll()
-        return results.compactMap { $0 }.filter { windowInfo in
-            // Filter out windows that have no appName or bundleID, which are likely system windows like the location services icon
-            let isSystemWindow = windowInfo.appName.isEmpty || windowInfo.bundleID.isEmpty
-                        
-            return !isSystemWindow
-        }
+        return results.compactMap { $0 }.filter { !$0.appName.isEmpty && !$0.bundleID.isEmpty }
     }
-
+    
+    /// Fetches detailed information for a given SCWindow.
     private static func fetchWindowInfo(window: SCWindow, applicationName: String) async throws -> WindowInfo? {
-        // Check if the window is in the default user space layer and is not fully transparent
         let windowID = window.windowID
         guard let windowInfoDict = CGWindowListCopyWindowInfo(.optionIncludingWindow, windowID) as? [[String: AnyObject]],
-              let windowLayer = windowInfoDict.first?[kCGWindowLayer as String] as? Int,
-              windowLayer == 0,
-              let windowAlpha = windowInfoDict.first?[kCGWindowAlpha as String] as? Double,
-              windowAlpha > 0,
+              let windowLayer = windowInfoDict.first?[kCGWindowLayer as String] as? Int, windowLayer == 0,
+              let windowAlpha = windowInfoDict.first?[kCGWindowAlpha as String] as? Double, windowAlpha > 0,
               let owningApplication = window.owningApplication else {
             return nil
         }
         
         let pid = owningApplication.processID
         let appRef = createAXUIElement(for: pid)
-        guard let windows = getAXWindows(for: appRef),
-              let title = window.title,
-              let windowRef = findWindow(byName: title, in: windows) else {
+        
+        guard let windows = getAXWindows(for: appRef), let title = window.title, let windowRef = findWindow(byName: title, in: windows) else {
             return nil
         }
         
         let closeButton = getCloseButton(for: windowRef)
         
-        var windowInfo = WindowInfo(
-            id: windowID,
-            window: window,
-            appName: owningApplication.applicationName,
-            bundleID: owningApplication.bundleIdentifier,
-            windowName: window.title,
-            image: nil,
-            axElement: windowRef,
-            closeButton: closeButton
-        )
+        var windowInfo = WindowInfo(id: windowID, window: window, appName: owningApplication.applicationName, bundleID: owningApplication.bundleIdentifier,
+                                    windowName: window.title, image: nil, axElement: windowRef, closeButton: closeButton, isMinimized: false)
         
         do {
             windowInfo.image = try await captureWindowImage(windowInfo: windowInfo)
