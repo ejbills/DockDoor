f6df90149b2c092dfb2c85d3807ab7945980b0c1
hasansultan92 <hasansultan92@hotmail.com>
2024-06-29 15:53:57 +0500

    Addition of segmented picker

--- DockDoor/Views/Settings/WindowSwitcher.swift
+++ DockDoor/Views/Settings/WindowSwitcher.swift
@@ -8,6 +8,7 @@
 
 import SwiftUI
 import Defaults
+import Cocoa
 
 struct WindowSwitcherSettingsView: View {
     @Default(.showWindowSwitcher) var showWindowSwitcher
@@ -20,9 +21,43 @@ struct WindowSwitcherSettingsView: View {
                 _, newValue in
                 restartApplication()
             }
-
+            // Default CMD + TAB implementation checkbox
+            ModifierKeyPickerView()
         }
         .padding(20)
         .frame(minWidth: 600)
+    }
+}
+
+struct ModifierKeyPickerView : View {
+    @State var modifierKey : String = ""
+    var body: some View {
+        VStack(spacing: 20) {
+            Picker("Modifier Key", selection: $modifierKey) {
+                ForEach(1...5, id: \.self) { modifierKeyId in
+                    Text(printModifierKey(for: modifierKeyId)).tag(modifierKeyId)
+                }
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            .onChange(of: modifierKey) { _, newValue in
+                // Change to the implementation here
+            }
+        }
+    }
+    
+    private func printModifierKey(for key: Int) -> String {
+        // Switch statement for presenting all the keys
+        switch key {
+        case 1:
+            return "Shift"
+        case 2:
+            return "Control"
+        case 3:
+            return "Option"
+        case 4:
+            return "Caps Lock"
+        default:
+            return "Command"
+        }
     }
 }
