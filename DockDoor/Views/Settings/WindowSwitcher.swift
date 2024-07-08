2285b77b1daa8a35e17030aefb88718ccd33c813
Hasan Sultan <52581466+hasansultan92@users.noreply.github.com>
2024-07-08 19:45:49 +0500

    Introducing the usage of custom keybinds set by user to switch windows (#88)
    * generate appcast script
    
    * Create draft_release.yml
    
    * Update draft_release.yml
    
    * Update draft_release.yml
    
    * Update draft_release.yml
    
    * Update draft_release.yml
    
    * Update draft_release.yml
    
    * Update draft_release.yml
    
    * Adding Setting pane to toggle window switcher
    
    * Addition of segmented picker
    
    * Initial Segmented Picker implementation
    
    * HotFix: Application was crashing when new user would launch
    
    * fallback assignment
    
    * Enabling usage of custom user keybinds
    
    * Deprecate use of UserDefaults in favor of Defaults
    
    * Remove: draft_release.yml
    
    ---------
    
    Co-authored-by: Ethan Bills <ethanjacksonbills@gmail.com>
    Co-authored-by: ejbills <74191134+ejbills@users.noreply.github.com>

--- DockDoor/Views/Settings/WindowSwitcher.swift
+++ DockDoor/Views/Settings/WindowSwitcher.swift
@@ -8,21 +8,108 @@
 
 import SwiftUI
 import Defaults
+import Carbon
+
+class KeybindModel: ObservableObject {
+    @Published var modifierKey: Int
+    @Published var isRecording: Bool = false
+    @Published var currentKeybind: UserKeyBind?
+
+    init() {
+        self.modifierKey = Defaults[.UserKeybind].modifierFlags
+        self.currentKeybind = Defaults[.UserKeybind]
+    }
+
+}
 
 struct WindowSwitcherSettingsView: View {
-    @Default(.showWindowSwitcher) var showWindowSwitcher
-    
+    @Default(.enableWindowSwitcher) var enableWindowSwitcher
+    @Default(.defaultCMDTABKeybind) var defaultCMDTABKeybind
     var body: some View {
         VStack(alignment: .leading, spacing: 10) {
-            Toggle(isOn: $showWindowSwitcher, label: {
+            Toggle(isOn: $enableWindowSwitcher, label: {
                 Text("Enable Window Switcher")
-            }).onChange(of: showWindowSwitcher){
+            }).onChange(of: enableWindowSwitcher){
                 _, newValue in
                 restartApplication()
             }
-
+            // Default CMD + TAB implementation checkbox
+            if Defaults[.enableWindowSwitcher] {
+                Toggle(isOn: $defaultCMDTABKeybind, label: {
+                    Text("Use default MacOS keybind ⌘ + ⇥")
+                })
+                // If default CMD Tab is not enabled
+                if !Defaults[.defaultCMDTABKeybind] {
+                    ModifierKeyPickerView()
+                }
+            }
         }
         .padding(20)
         .frame(minWidth: 600)
     }
+}
+
+struct ModifierKeyPickerView : View {
+    @ObservedObject var viewModel = KeybindModel()
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Picker("Modifier Key", selection: $viewModel.modifierKey) {
+                Text("Control (⌃)").tag(Defaults[.Int64maskControl])
+                Text("Option (⌥)").tag(Defaults[.Int64maskAlternate])
+                Text("Command (⌘)").tag(Defaults[.Int64maskCommand])
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            Text("Press any key combination to set the keybind").padding()
+            Button(action: {viewModel.isRecording = true}){
+            Text(viewModel.isRecording ? "Press key ..." : "Record keybind")
+            }.keyboardShortcut(.defaultAction)
+            if let keybind = viewModel.currentKeybind {
+                Text("Current Keybind: \(printCurrentKeybind(keybind))").padding()
+            }
+        }
+        .background(ShortcutCaptureView(currentKeybind: $viewModel.currentKeybind, isRecording: $viewModel.isRecording, modifierKey: $viewModel.modifierKey))
+        .onAppear{
+            viewModel.currentKeybind = Defaults[.UserKeybind]
+        }
+        .frame(alignment: .leading)
+    }
+    
+    func printCurrentKeybind(_ shortcut: UserKeyBind) -> String {
+        var parts: [String] = []
+        parts.append(modifierConverter.toString(shortcut.modifierFlags))
+        parts.append(KeyCodeConverter.toString(shortcut.keyCode))
+        return parts.joined(separator: " ")
+    }
+    
+}
+
+struct ShortcutCaptureView: NSViewRepresentable {
+    @Binding var currentKeybind: UserKeyBind?
+    @Binding var isRecording: Bool
+    @Binding var modifierKey: Int
+    
+    func makeNSView(context: Context) -> NSView {
+        let view = NSView()
+        NSEvent.addLocalMonitorForEvents(matching: .keyDown) { event in
+            guard self.isRecording else {
+                return event
+            }
+            self.isRecording = false
+            if event.keyCode == 48 && modifierKey == Defaults[.Int64maskCommand] { // User has chosen the default Mac OS window switcher keybind
+                // Set the default CMDTAB
+                Defaults[.defaultCMDTABKeybind] = true
+                Defaults[.UserKeybind] = UserKeyBind(keyCode: 48, modifierFlags: Defaults[.Int64maskControl])
+                self.currentKeybind = Defaults[.UserKeybind]
+                return event
+            }
+            Defaults[.UserKeybind] = UserKeyBind(keyCode: event.keyCode, modifierFlags: modifierKey)
+            self.currentKeybind = Defaults[.UserKeybind]
+            return nil
+        }
+        return view
+    }
+    
+    
+    func updateNSView(_ nsView: NSView, context: Context) {}
 }
