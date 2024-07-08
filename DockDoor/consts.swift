a46f9cf20fd4b936d778659ab769948644285c09
ejbills <74191134+ejbills@users.noreply.github.com>
2024-07-08 07:58:15 -0700

    Feature: Add window switcher keybind customization (#91)
    * Introducing the usage of custom keybinds set by user to switch windows (#88)
    
    * generate appcast script
    
    * Create draft_release.yml
    
    * Update draft_release.yml
    
    * Update draft_release.yml
    
    * Update draft_release.yml
    
    * Update draft_release.yml
    
    * Update draft_release.yml
    
    * Update draft_release.yml
    
    * Adding Setting pane to toggle window switcher
    
    * Addition of segmented picker
    
    * Initial Segmented Picker implementation
    
    * HotFix: Application was crashing when new user would launch
    
    * fallback assignment
    
    * Enabling usage of custom user keybinds
    
    * Deprecate use of UserDefaults in favor of Defaults
    
    * Remove: draft_release.yml
    
    ---------
    
    Co-authored-by: Ethan Bills <ethanjacksonbills@gmail.com>
    Co-authored-by: ejbills <74191134+ejbills@users.noreply.github.com>
    
    * fix window view
    
    * fix window title
    
    ---------
    
    Co-authored-by: Hasan Sultan <52581466+hasansultan92@users.noreply.github.com>

--- DockDoor/consts.swift
+++ DockDoor/consts.swift
@@ -7,6 +7,7 @@
 
 import Cocoa
 import Defaults
+//import Carbon
 
 let optimisticScreenSizeWidth = NSScreen.main!.frame.width
 let optimisticScreenSizeHeight = NSScreen.main!.frame.height
@@ -19,8 +20,12 @@ extension Defaults.Keys {
     static let openDelay = Key<CGFloat>("openDelay") { 0 }
     static let screenCaptureCacheLifespan = Key<CGFloat>("screenCaptureCacheLifespan") { 60 }
     static let showAnimations = Key<Bool>("showAnimations") { true }
-    static let showWindowSwitcher = Key<Bool>("showWindowSwitcher"){ true }
+    static let enableWindowSwitcher = Key<Bool>("enableWindowSwitcher"){ true }
     static let showMenuBarIcon = Key<Bool>("showMenuBarIcon", default: true)
-    
+    static let defaultCMDTABKeybind = Key<Bool>("defaultCMDTABKeybind") { true }
     static let launched = Key<Bool>("launched") { false }
+    static let Int64maskCommand = Key<Int>("Int64maskCommand") { 1048840 }
+    static let Int64maskControl = Key<Int>("Int64maskControl") { 262401 }
+    static let Int64maskAlternate = Key<Int>("Int64maskAlternate") { 524576 }
+    static let UserKeybind = Key<UserKeyBind>("UserKeybind", default: UserKeyBind(keyCode: 48, modifierFlags: Defaults[.Int64maskControl]))
 }
