0a4879739c9aff3b47ff5f273b8ccc6b11cc13d3
hasansultan92 <hasansultan92@hotmail.com>
2024-07-26 01:17:39 +0500

    Refactor: Remove unnecessary private API definitions

--- DockDoor/Utilities/PrivateApis.swift
+++ DockDoor/Utilities/PrivateApis.swift
@@ -7,25 +7,7 @@
 
 import Cocoa
 
-typealias ScreenUuid = CFString
-typealias CGSConnectionID = UInt32
-typealias CGSSpaceID = UInt64
-
 // returns the CGWindowID of the provided AXUIElement
 // * macOS 10.10+
 @_silgen_name("_AXUIElementGetWindow") @discardableResult
 func _AXUIElementGetWindow(_ axUiElement: AXUIElement, _ wid: inout CGWindowID) -> AXError
-
-// get the display UUID with the active menubar (other menubar are dimmed)
-@_silgen_name("CGSCopyActiveMenuBarDisplayIdentifier")
-func CGSCopyActiveMenuBarDisplayIdentifier(_ cid: CGSConnectionID) -> ScreenUuid
-
-// returns the current space ID on the provided display UUID
-// * macOS 10.10+
-@_silgen_name("CGSManagedDisplayGetCurrentSpace")
-func CGSManagedDisplayGetCurrentSpace(_ cid: CGSConnectionID, _ displayUuid: ScreenUuid) -> CGSSpaceID
-
-// returns the connection to the WindowServer. This connection ID is required when calling other APIs
-// * macOS 10.10+
-@_silgen_name("CGSMainConnectionID")
-func CGSMainConnectionID() -> CGSConnectionID
