c930148e3b36182ee42fb08a6363f4d330ecb403
illavoluntas <174935274+illavoluntas@users.noreply.github.com>
2024-08-31 20:16:22 +0200

    fix: light/dark mode toggle button showing ambiguous text

--- dark-mode.js
+++ dark-mode.js
@@ -1,14 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
     const toggleDarkModeButton = document.getElementById('toggleDarkMode');
+    const darkModeIcon = document.getElementById('darkModeIcon');
     const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
     const savedTheme = localStorage.getItem('theme');
 
     // If user already has saved setting
     if (savedTheme) {
         document.body.classList.toggle('dark-mode', savedTheme === 'dark');
+        darkModeIcon.textContent = savedTheme === 'dark' ? '🌙' : '☀️';
     } else if (prefersDarkMode) {
         // Else, use system setting
         document.body.classList.add('dark-mode');
+        darkModeIcon.textContent = '🌙';
+    } else {
+        darkModeIcon.textContent = '☀️';
     }
 
     toggleDarkModeButton.addEventListener('click', function () {
@@ -17,7 +22,6 @@ document.addEventListener('DOMContentLoaded', function () {
         localStorage.setItem('theme', theme);
     
         // Change icon according to current theme
-        const darkModeIcon = document.getElementById('darkModeIcon');
         darkModeIcon.textContent = theme === 'dark' ? '🌙' : '☀️';
     });
-})
+});
