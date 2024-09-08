89505ab419cb59ac3cebcf2960d492f9d65d10ff
Igor Marcossi <igor@lo.cafe>
2024-09-07 23:13:14 -0300

    Progress

---
+++ DockDoor/Views/Intro/FirstTimeViewInstructionsView.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+
+struct FirstTimeViewInstructionsView: View {
+    var step: Int = 0
+    var action: () -> Void = {}
+    var body: some View {
+        VStack(spacing: 8) {
+            if step >= 1 {
+                Text("Welcome to DockDoor!")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .transition(FirstTimeView.transition)
+            }
+
+            if step >= 2 {
+                Text("Enhance your dock experience!")
+                    .font(.system(size: 16))
+                    .foregroundColor(.secondary)
+                    .transition(FirstTimeView.transition)
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .scaleEffect(1)
+
+        if step >= 3 {
+            Button("Get Started") {
+                action()
+            }
+            .buttonStyle(AccentButtonStyle())
+            .transition(FirstTimeView.transition)
+        }
+    }
+}
+
+#Preview {
+    FirstTimeViewInstructionsView()
+}
