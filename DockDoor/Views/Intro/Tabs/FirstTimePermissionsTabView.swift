9f4efaa2a49088630d588ded5cac3f213976ef8f
Igor Marcossi <igor@lo.cafe>
2024-09-08 02:26:09 -0300

    Adding title to intro

--- DockDoor/Views/Intro/Tabs/FirstTimePermissionsTabView.swift
+++ DockDoor/Views/Intro/Tabs/FirstTimePermissionsTabView.swift
@@ -3,10 +3,13 @@ import SwiftUI
 struct FirstTimePermissionsTabView: View {
     var nextTab: () -> Void
     var body: some View {
-        ZStack {
+        VStack(spacing: 16) {
+            Text("May I have some permissions?")
+                .font(.largeTitle)
+                .fontWeight(.bold)
             PermissionsView()
-                .padding(32)
         }
+        .padding(32)
     }
 }
 
