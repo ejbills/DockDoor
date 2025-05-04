0af80090ae76ded1be6e66e705697e8db7fe32e0
ejbills <74191134+ejbills@users.noreply.github.com>
2025-05-04 09:28:59 -0700

    fix: Optimize window switcher performance (#532)
    * chore: v1.7.1
    
    * fix: optimize off thread
    
    * fix: hide window earlier

--- DockDoor/Utilities/Window Management/SpaceWindowCacheManager.swift
+++ DockDoor/Utilities/Window Management/SpaceWindowCacheManager.swift
@@ -3,47 +3,47 @@ import Defaults
 import ScreenCaptureKit
 
 class SpaceWindowCacheManager {
-    private let queue = DispatchQueue(label: "com.dockdoor.cacheQueue", attributes: .concurrent)
-    private var desktopSpaceWindowCache: [pid_t: Set<WindowInfo>] = [:]
+    private var windowCache: [pid_t: Set<WindowInfo>] = [:]
+    private let cacheLock = NSLock()
 
     func readCache(pid: pid_t) -> Set<WindowInfo> {
-        queue.sync {
-            desktopSpaceWindowCache[pid] ?? []
-        }
+        cacheLock.lock()
+        defer { cacheLock.unlock() }
+        return windowCache[pid] ?? []
     }
 
     func writeCache(pid: pid_t, windowSet: Set<WindowInfo>) {
-        queue.async(flags: .barrier) {
-            self.desktopSpaceWindowCache[pid] = windowSet
-        }
+        cacheLock.lock()
+        defer { cacheLock.unlock() }
+        windowCache[pid] = windowSet
     }
 
-    func updateCache(pid: pid_t, update: @escaping (inout Set<WindowInfo>) -> Void) {
-        queue.async(flags: .barrier) {
-            var windowSet = self.desktopSpaceWindowCache[pid] ?? []
-            update(&windowSet)
-            self.desktopSpaceWindowCache[pid] = windowSet
-        }
+    func updateCache(pid: pid_t, update: (inout Set<WindowInfo>) -> Void) {
+        cacheLock.lock()
+        defer { cacheLock.unlock() }
+        var windowSet = windowCache[pid] ?? []
+        update(&windowSet)
+        windowCache[pid] = windowSet
     }
 
     func removeFromCache(pid: pid_t, windowId: CGWindowID) {
-        queue.async(flags: .barrier) {
-            if var windowSet = self.desktopSpaceWindowCache[pid],
-               let windowToRemove = windowSet.first(where: { $0.id == windowId })
-            {
-                windowSet.remove(windowToRemove)
-                if windowSet.isEmpty {
-                    self.desktopSpaceWindowCache.removeValue(forKey: pid)
-                } else {
-                    self.desktopSpaceWindowCache[pid] = windowSet
-                }
+        cacheLock.lock()
+        defer { cacheLock.unlock() }
+        if var windowSet = windowCache[pid],
+           let windowToRemove = windowSet.first(where: { $0.id == windowId })
+        {
+            windowSet.remove(windowToRemove)
+            if windowSet.isEmpty {
+                windowCache.removeValue(forKey: pid)
+            } else {
+                windowCache[pid] = windowSet
             }
         }
     }
 
     func getAllWindows() -> [WindowInfo] {
-        queue.sync {
-            self.desktopSpaceWindowCache.values.flatMap(\.self).sorted(by: { $0.date > $1.date })
-        }
+        cacheLock.lock()
+        defer { cacheLock.unlock() }
+        return Array(windowCache.values.flatMap { $0 }).sorted(by: { $0.date > $1.date })
     }
 }
