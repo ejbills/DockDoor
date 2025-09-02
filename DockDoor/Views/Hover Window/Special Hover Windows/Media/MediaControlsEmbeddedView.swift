3555e399243f90ad1011f96e88e53c74122dd7a3
Ethan <74191134+ejbills@users.noreply.github.com>
2025-09-02 12:05:03 -0700

    media: add pinning context menu to embedded media controls

--- DockDoor/Views/Hover Window/Special Hover Windows/Media/MediaControlsEmbeddedView.swift
+++ DockDoor/Views/Hover Window/Special Hover Windows/Media/MediaControlsEmbeddedView.swift
@@ -19,6 +19,7 @@ struct MediaControlsEmbeddedView: View {
             .padding(12)
             .frame(minWidth: idealWidth ?? (MediaControlsLayout.embeddedArtworkSize + MediaControlsLayout.artworkTextSpacing + 165), alignment: .center)
             .simpleBlurBackground(strokeWidth: 1.75)
+            .pinnable(appName: appName, bundleIdentifier: bundleIdentifier, type: .media)
     }
 
     @ViewBuilder
