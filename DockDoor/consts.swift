7e4db8b820417b9a36483c78ab3fb676123cd272
Ethan <74191134+ejbills@users.noreply.github.com>
2025-12-05 18:10:42 -0800

    fix: remove deprecated sort windows by date option

--- DockDoor/consts.swift
+++ DockDoor/consts.swift
@@ -62,7 +62,6 @@ extension Defaults.Keys {
     static let scrollToMouseHoverInSwitcher = Key<Bool>("scrollToMouseHoverInSwitcher", default: false)
     static let keepPreviewOnAppTerminate = Key<Bool>("keepPreviewOnAppTerminate", default: false)
     static let enableWindowSwitcherSearch = Key<Bool>("enableWindowSwitcherSearch", default: false)
-    static let sortWindowsByDate = Key<Bool>("sortWindowsByDate", default: true)
     static let useClassicWindowOrdering = Key<Bool>("useClassicWindowOrdering", default: true)
     static let includeHiddenWindowsInSwitcher = Key<Bool>("includeHiddenWindowsInSwitcher", default: true)
     static let ignoreAppsWithSingleWindow = Key<Bool>("ignoreAppsWithSingleWindow", default: false)
