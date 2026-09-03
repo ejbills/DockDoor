d99a075e6026f77736604987ac599d3ebb88942f
Ethan <74191134+ejbills@users.noreply.github.com>
2026-09-03 14:29:52 -0700

    Revert "fix: handle blank-area preview taps in AppKit so card clicks are not stolen by the container gesture, closes #1586, closes #1584"
    This reverts commit 5845f961b706b8d9504bb5a3c4f129aec039778e.

--- DockDoor/Views/Hover Window/WindowPreviewHoverContainer.swift
+++ DockDoor/Views/Hover Window/WindowPreviewHoverContainer.swift
@@ -277,6 +277,10 @@ struct WindowPreviewHoverContainer: View {
             content: { windowGridContent() },
             backgroundAppearance: backgroundAppearance
         )
+        .contentShape(Rectangle())
+        .onTapGesture {
+            activateApp()
+        }
         .padding(.top, (!previewStateCoordinator.windowSwitcherActive && effectiveAppNameStyle == .popover && effectiveShowAppName) ? 30 : 0)
         .onAppear {
             loadAppIcon()
@@ -737,7 +741,6 @@ struct WindowPreviewHoverContainer: View {
                 coordinator: previewStateCoordinator,
                 scrolledFromStart: $scrolledFromStart,
                 isInteractive: !mockPreviewActive,
-                onBackgroundTap: activateApp,
                 makeCard: { item in
                     AnyView(buildFlowItem(
                         item: item,
