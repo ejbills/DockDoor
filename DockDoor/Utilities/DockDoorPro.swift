48483a7704a2430dceabd9465b65434dfbb3bff0
Ethan <74191134+ejbills@users.noreply.github.com>
2026-09-18 09:22:00 -0700

    Update DockDoor Pro icon and fix its broken remote icon URL

--- DockDoor/Utilities/DockDoorPro.swift
+++ DockDoor/Utilities/DockDoorPro.swift
@@ -2,7 +2,7 @@ import AppKit
 
 enum DockDoorPro {
     static let url = URL(string: "https://pro.dockdoor.net")!
-    static let iconURL = URL(string: "https://pro.dockdoor.net/_astro/dockdoor-icon.DNTkj7IN_GoFVN.webp")!
+    static let iconURL = URL(string: "https://pro.dockdoor.net/apple-touch-icon.png")!
 
     static func open() {
         NSWorkspace.shared.open(url)
